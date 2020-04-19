a3365bb8178e07b0adca7ea90b53031a7a4a81bb
面川泰明 <omokawa@senk-inc.co.jp>
2020-04-19 12:47:05 +0900

    Add header from element ui

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,16 @@
 <template>
   <section class="container">
-    
+    <el-card style="flex: 1">
+      <div slot="header" class="clearfix">
+        <span>ログイン</span>
+      </div>
+    </el-card>
   </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
