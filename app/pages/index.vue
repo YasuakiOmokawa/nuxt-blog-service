ac398bbe62afdcbb74561d64c66814906bff10d4
面川泰明 <omokawa@senk-inc.co.jp>
2020-04-25 11:59:10 +0900

    Complete index view for sign-in or sign-up

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,15 +9,37 @@
           <span>ユーザー ID</span>
           <el-input v-model="formData.id" placeholder />
         </div>
-        <div class="form-content"></div>
+        <div class="form-content">
+          <el-checkbox v-model="isCreateMode">アカウントを作成する</el-checkbox>
+        </div>
+        <div class="text-right">
+          <el-button type="primary">{{buttonText}}</el-button>
+        </div>
       </form>
     </el-card>
   </section>
 </template>
 
 <script>
-export default {}
+export default {
+  asyncData() {
+    return {
+      isCreateMode: false,
+      formData: {
+        id: ""
+      }
+    }
+  },
+  computed: {
+    buttonText() {
+      return this.isCreateMode ? "新規登録" : "ログイン"
+    }
+  }
+}
 </script>
 
-<style>
+<style scoped>
+.form-content {
+  margin: 16px 0;
+}
 </style>
