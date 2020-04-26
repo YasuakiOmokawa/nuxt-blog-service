0bfb34c447571cc5d4efa0f47f53c644918b7701
面川泰明 <omokawa@senk-inc.co.jp>
2020-04-26 13:54:54 +0900

    Coding posts index
    under coding

---
+++ app/pages/posts/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="container posts-page">
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>新着投稿</span>
+      </div>
+      <el-table
+        :data="showPosts"
+        style="width: 100%"
+        class="table"
+      >
+        <el-table-column
+          label="タイトル"
+          prop="title"
+        >
+        </el-table-column>
+        <el-table-column
+          label="投稿者"
+          prop="user.id"
+          width="180"
+        >
+        </el-table-column>
+        <el-table-column
+          label="投稿日時"
+          prop="created_at"
+          width="240"
+        >
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    showPosts() {
+      return [
+        {
+
+        }
+      ]
+    }
+  }
+}
+</script>
