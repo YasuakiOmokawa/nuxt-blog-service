3efa62d29901060cf050f2009d2db3d741822b2a
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-03 13:37:23 +0900

    Finish top index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,7 +13,7 @@
           <el-checkbox v-model="isCreateMode">アカウントを作成する</el-checkbox>
         </div>
         <div class="text-right">
-          <el-button type="primary">{{buttonText}}</el-button>
+          <el-button type="primary" @click="handleClickSubmit">{{buttonText}}</el-button>
         </div>
       </form>
     </el-card>
@@ -21,8 +21,13 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex"
+
 export default {
-  asyncData() {
+  asyncData({ redirect, store }) {
+    if (store.getters["user"]) {
+      redirect("/posts/")
+    }
     return {
       isCreateMode: false,
       formData: {
@@ -34,6 +39,50 @@ export default {
     buttonText() {
       return this.isCreateMode ? "新規登録する" : "ログインする"
     }
+  },
+  methods: {
+    async handleClickSubmit() {
+      if (this.isCreateMode) {
+        try {
+          await this.register({ ...this.formData })
+          this.$notify({
+            type: "success",
+            title: "アカウント作成完了",
+            message: `${this.formData.id} として登録しました`,
+            position: "bottom-right",
+            duration: 1000
+          })
+          this.$router.push("/posts/")
+        } catch (e) {
+          this.$notify.error({
+            title: "アカウント作成失敗",
+            message: "既に登録されているか、不正なユーザー ID です",
+            position: "bottom-right",
+            duration: 1000
+          })
+        }
+      } else {
+        try {
+          await this.login({ ...this.formData })
+          this.$notify({
+            type: "success",
+            title: "ログイン成功",
+            message: `${this.formData.id} としてログインしました`,
+            position: "bottom-right",
+            duration: 1000
+          })
+          this.$router.push("/posts/")
+        } catch (e) {
+          this.$notify.error({
+            title: "ログイン失敗",
+            message: "不正なユーザー ID です",
+            position: "bottom-right",
+            duration: 1000
+          })
+        }
+      }
+    },
+    ...mapActions(["login", "register"])
   }
 }
 </script>
--- app/store/index.js
+++ app/store/index.js
@@ -1,28 +1,32 @@
 export const state = () => ({
   isLoggedIn: false,
   user: null
-})
+});
 
 export const getters = {
-  isLoggedIn: (state) => state.isLoggedIn,
-  user: (state) => state.user
-}
+  isLoggedIn: state => state.isLoggedIn,
+  user: state => state.user
+};
 
 export const mutations = {
   setUser(state, { user }) {
-    state.user = user
-    state.isLoggedIn = true
+    state.user = user;
+    state.isLoggedIn = true;
   }
-}
+};
 
 export const actions = {
-  async login({commit}, {id}) {
-    const user = await this.$axios.$get('/users/${id}.json')
-    if (!user.id) throw new Error('Invalid user')
-    commit('setUser', {user})
+  async login({ commit }, { id }) {
+    const user = await this.$axios.$get("/users/${id}.json");
+    if (!user.id) throw new Error("Invalid user");
+    commit("setUser", { user });
   },
-  async register({commit}, {id}) {
-    const payload = {}
-    
+  async register({ commit }, { id }) {
+    const payload = {};
+    payload[id] = { id };
+    await this.$axios.$patch(`/users.json`, payload);
+    const user = await this.$axios.$get(`/users/${id}.json`);
+    if (!user.id) throw new Error("Invalid user");
+    commit("setUser", { user });
   }
-}
+};
