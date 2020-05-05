e51e6b7196c6662864f56b37c65b9e86ca2713ce
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-05 11:10:56 +0900

    View user logging on posts page

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -3,17 +3,24 @@
     <el-menu-item index="1" style="pointer-events:none;">Nuxt Diary App</el-menu-item>
     <el-menu-item index="2" :route="{ path: '/posts/' }">投稿一覧</el-menu-item>
 
-    <no-ssr>
-      <el-menu-item index="4" style="float: right;" :route="{ path: '/' }">
-        <span>ログイン</span>
-      </el-menu-item>
-    </no-ssr>
+    <el-menu-item index="4" style="float: right;" :route="{path: `/users/{user.id}`}" v-if="user">
+      <span>{{user.id}}</span>
+    </el-menu-item>
+    <el-menu-item index="4" style="float: right;" :route="{ path: '/' }" v-else>
+      <span>ログイン</span>
+    </el-menu-item>
     <el-menu-item index="5" :route="{ path: '/posts/new' }">新規投稿</el-menu-item>
   </el-menu>
 </template>
 
 <script>
-export default {}
+import { mapGetters } from "vuex"
+
+export default {
+  computed: {
+    ...mapGetters(["user"])
+  }
+}
 </script>
 
 <style>
