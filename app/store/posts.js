e61b82e883e71078a1f2071051438af9f16fb6dd
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-10 11:37:13 +0900

    Create posts logic

---
+++ app/store/posts.js
@@ -0,0 +1,37 @@
+import moment from "~/plugins/moment";
+
+export const state = () => ({
+  posts: []
+});
+
+export const getters = {
+  posts: state => state.posts
+};
+
+export const mutations = {
+  addPost(state, { post }) {
+    state.posts.push(post);
+  },
+  updatePost(state, { post }) {
+    state.posts = state.posts.map(p => (p.id === post.id ? post : p));
+  },
+  clearPosts(state) {
+    state.posts = [];
+  }
+};
+
+export const actions = {
+  async publishPost({ commit }, { payload }) {
+    const user = await this.$axios.$get(`/users/${payload.user.id}.json`);
+    const post_id = (await this.$axios.$post("/posts.json", payload)).name;
+    const created_at = moment().format();
+    const post = { id: post_id, ...payload, created_at };
+    const putData = { id: post_id, ...payload, created_at };
+    delete putData.user;
+    await this.$axios.$put(`/users/${user.id}/posts.json`, [
+      ...(user.posts || []),
+      putData
+    ]);
+    commit("addPost", { post });
+  }
+};
