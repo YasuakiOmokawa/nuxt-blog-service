10c57bdd56f46ae65cbdea1c667180d6bb5f81cc
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-10 11:36:57 +0900

    Create new view to posts

---
+++ app/pages/posts/new.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="container posts-page">
+    <el-card style="flex: 1">
+      <div slot="header" class="clearfix">
+        <el-input v-model="formData.title" placeholder="タイトルを入力" />
+      </div>
+      <div>
+        <el-input v-model="formData.body" placeholder="本文を入力......" type="textarea" rows="15" />
+      </div>
+      <div class="text-right" style="margin-top: 16px;">
+        <el-button type="primary" @click="publish" round>
+          <span class="el-icon-upload2" />
+          <span>Publish</span>
+        </el-button>
+      </div>
+    </el-card>
+  </section>
+</template>
+<script>
+import { mapGetters, mapActions } from "vuex"
+
+export default {
+  asyncData({ redirect, store }) {
+    if (!store.getters["user"]) {
+      redirect("/")
+    }
+    return {
+      formData: {
+        title: "",
+        body: ""
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(["user"])
+  },
+  methods: {
+    publish() {}
+  }
+}
+</script>
+
+<style>
+.posts-page .el-table__row {
+  cursor: pointer;
+}
+</style>
