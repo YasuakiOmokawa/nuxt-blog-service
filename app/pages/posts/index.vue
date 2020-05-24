87915be2eae30012fae397d02f5f6f31a7ec8c92
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-24 13:16:38 +0900

    Bind click to go to detail page

--- app/pages/posts/index.vue
+++ app/pages/posts/index.vue
@@ -4,7 +4,7 @@
       <div slot="header" class="clearfix">
         <span>新着投稿</span>
       </div>
-      <el-table :data="showPosts" style="width: 100%" class="table">
+      <el-table :data="showPosts" style="width: 100%" class="table" @row-click="handleClick">
         <el-table-column label="タイトル" prop="title"></el-table-column>
         <el-table-column label="投稿者" prop="user.id" width="180"></el-table-column>
         <el-table-column label="投稿日時" prop="created_at" width="240"></el-table-column>
