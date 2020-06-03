b84b9c3ec6075cf430e8adef7025d91abd3c375e
面川泰明 <omokawa@senk-inc.co.jp>
2020-06-03 09:56:18 +0900

    Add users page

---
+++ app/pages/users/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <el-row v-if="user">
+      <el-col :span="6">
+        <el-card class="text-center" style="margin-right: 16px;">
+          <div>
+            <img src="https://placehold.it/150x150"
+            style="witdh: 100%;margin-bottom:16px;"
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
