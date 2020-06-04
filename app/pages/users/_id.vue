a5241b511ed2d3ebec43a1731deb8562f4247079
面川泰明 <omokawa@senk-inc.co.jp>
2020-06-04 09:55:54 +0900

    Coding user detail page

--- app/pages/users/_id.vue
+++ app/pages/users/_id.vue
@@ -5,9 +5,17 @@
         <el-card class="text-center" style="margin-right: 16px;">
           <div>
             <img src="https://placehold.it/150x150"
-            style="witdh: 100%;margin-bottom:16px;"
+            style="witdh: 100%;margin-bottom: 16px;border-radius: 2px;"
+            alt=""
+            />
           </div>
+          <h2>
+            <b>{{user.id}}</b>
+          </h2>
         </el-card>
+      </el-col>
+      <el-col :span="18">
+
       </el-col>
     </el-row>
   </div>
