f247f090dae6b106fa5dae85c2096826fe899dfd
面川泰明 <omokawa@senk-inc.co.jp>
2020-06-09 09:54:13 +0900

    Fix typo

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -3,7 +3,7 @@
     <el-menu-item index="1" style="pointer-events:none;">Nuxt Diary App</el-menu-item>
     <el-menu-item index="2" :route="{ path: '/posts/' }">投稿一覧</el-menu-item>
 
-    <el-menu-item index="4" style="float: right;" :route="{path: `/users/{user.id}`}" v-if="user">
+    <el-menu-item index="4" style="float: right;" :route="{path: `/users/${user.id}`}" v-if="user">
       <span>{{user.id}}</span>
     </el-menu-item>
     <el-menu-item index="4" style="float: right;" :route="{ path: '/' }" v-else>
